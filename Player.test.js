512d53e9dbd4bd65f6e7e10b2a58bd7e03eee394
Kálmán Tarnay <kalman@tarnay.me>
2019-10-05 13:41:58 +0200

    test fix

--- Player.test.js
+++ Player.test.js
@@ -1,12 +1,17 @@
 const sinon = require('sinon')
 const Player = require('./Player.js')
-const defaultGameState = require('./game-state.json')
+const _defaultGameState = require('./game-state.json')
+
+function getDefaultGameState() {
+  return JSON.parse(JSON.stringify(_defaultGameState));
+}
 
 const betMock = sinon.stub()
 
 test('should not go all-in with low pair in hand', () => {
   // given
-  const gameState = defaultGameState
+  const gameState = getDefaultGameState()
+  gameState.community_cards = [];
   gameState.players[gameState.in_action].hole_cards[0] = { "rank": "6", "suit": "hearts" }
   gameState.players[gameState.in_action].hole_cards[1] = { "rank": "6", "suit": "spades" }
   gameState.current_buy_in = 20
@@ -22,7 +27,8 @@ test('should not go all-in with low pair in hand', () => {
 
 test('should go all-in with high pair in hand', () => {
   // given
-  const gameState = defaultGameState
+  const gameState = getDefaultGameState()
+  gameState.community_cards = [];
   gameState.players[gameState.in_action].hole_cards[0] = { "rank": "K", "suit": "hearts" }
   gameState.players[gameState.in_action].hole_cards[1] = { "rank": "K", "suit": "spades" }
 
@@ -35,7 +41,8 @@ test('should go all-in with high pair in hand', () => {
 
 test('should fold with low pair when another player is all in', () => {
   // given
-  const gameState = defaultGameState
+  const gameState = getDefaultGameState()
+  gameState.community_cards = [];
   gameState.players[gameState.in_action].hole_cards[0] = { "rank": "6", "suit": "hearts" }
   gameState.players[gameState.in_action].hole_cards[1] = { "rank": "6", "suit": "spades" }
   gameState.current_buy_in = 20
