9c0b2e229f641f4986595cc599e49f56fe28cde8
tbalint19 <toth910719balint@gmail.com>
2019-10-05 11:08:00 +0200

    first test with sinon

--- Player.test.js
+++ Player.test.js
@@ -1,3 +1,31 @@
-test('1 + 1 to equal 2', () => {
-  expect(1 + 1).toBe(2);
-});
+const sinon = require('sinon')
+const Player = require('./Player.js')
+const defaultGameState = require('./game-state.json')
+
+const betMock = sinon.stub()
+
+test('should go all-in with pair in hand', () => {
+  // given
+  const gameState = defaultGameState
+  gameState.players[gameState.in_action].hole_cards[0] = { "rank": "K", "suit": "hearts" }
+  gameState.players[gameState.in_action].hole_cards[1] = { "rank": "K", "suit": "spades" }
+
+  // when
+  Player.betRequest(gameState, betMock)
+
+  // then
+  sinon.assert.calledWith(betMock, 4000);
+})
+
+// test('should go all-in with pair in hand', () => {
+//   // given
+//   const gameState = defaultGameState
+//   gameState.players[gameState.in_action].hole_cards[0] = { "rank": "6", "suit": "hearts" }
+//   gameState.players[gameState.in_action].hole_cards[1] = { "rank": "6", "suit": "spades" }
+//
+//   // when
+//   Player.betRequest(gameState, betMock)
+//
+//   // then
+//   sinon.assert.calledWith(betMock, 4000);
+// })
